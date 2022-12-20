edae73b8774d3692d4fd2bed3c923a92ffce7634
Yadhu <yadhu621@gmail.com>
2022-12-20 15:01:34 +0000

    add index.html

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
     $('#submit-btn').on('click',function(){
         console.log("Hello, you clicked submit button")
+
+        var firstName = $('#firstName').val()
+        console.log(firstName)
     })
 })
